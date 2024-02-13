375b7699187130e85b0badeab43da82a700057d5
Wahid Alimi <wahid.alimi@gmail.com>
2024-02-13 08:25:37 -0500

    Image classification. Update storage account URL.

--- Labfiles/02-image-classification/replace.ps1
+++ Labfiles/02-image-classification/replace.ps1
@@ -1,2 +1,2 @@
-$storageAcct = '<storageAccount>'
+$storageAcct = 'waai102visionservice'
 (Get-Content training-images/training_labels.json) -replace '<storageAccount>', $storageAcct | Out-File training-images/training_labels.json
