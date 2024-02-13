b5b70d0e9fb86592ea89b8b306381985f39e2e6b
Wahid Alimi <wahid.alimi@gmail.com>
2024-02-13 08:26:47 -0500

    Lab 4: Using AI Vision service to detect faces.

--- Labfiles/04-face/C-Sharp/computer-vision/Program.cs
+++ Labfiles/04-face/C-Sharp/computer-vision/Program.cs
@@ -5,7 +5,8 @@ using Azure;
 using System.IO;
 
 // Import namespaces
-
+using Azure.AI.Vision.Common;
+using Azure.AI.Vision.ImageAnalysis;
 
 namespace detect_people
 {
@@ -30,6 +31,8 @@ namespace detect_people
                 }
 
                 // Authenticate Azure AI Vision client
+                var cvClient = new VisionServiceOptions(aiSvcEndpoint, new AzureKeyCredential(aiSvcKey));
+
 
                 
                 // Analyze image
@@ -48,14 +51,60 @@ namespace detect_people
 
             var analysisOptions = new ImageAnalysisOptions()
             {
-                // Specify features to be retrieved
-
+                // Specify features to be retrieved (PEOPLE)
+                Features = ImageAnalysisFeature.People
             };
 
             // Get image analysis
+            using var imageSource = VisionSource.FromFile(imageFile);
 
+            using var analyzer = new ImageAnalyzer(serviceOptions, imageSource, analysisOptions);
+
+            var result = analyzer.Analyze();
+
+            if (result.Reason == ImageAnalysisResultReason.Analyzed)
+            {
+                // Get people in the image
+                if (result.People != null)
+                {
+                    Console.WriteLine($"    People:");
+
+                    // Prepare image for drawing
+                    System.Drawing.Image image = System.Drawing.Image.FromFile(imageFile);
+                    Graphics graphics = Graphics.FromImage(image);
+                    Pen pen = new Pen(Color.Cyan, 3);
+                    Font font = new Font("Arial", 16);
+                    SolidBrush brush = new SolidBrush(Color.WhiteSmoke);
+
+                    foreach (var person in result.People)
+                    {
+                        // Draw object bounding box if confidence is greater than 50%
+                        if (person.Confidence > 0.5)
+                        {
+                            // Draw object bouding box
+                            var r = person.BoundingBox;
+                            Rectangle rect = new Rectangle(r.X, r.Y, r.Width, r.Height);
+                            graphics.DrawRectangle(pen, rect);
+
+                            // Return the confidence of the person detected
+                            Console.WriteLine($"    Bounding box {person.BoundingBox}, Confidence {person.Confidence:0.0000}");
+                        }
+                    }
+
+                    // Save annotated image
+                    string output_file = "detected_people.jpg";
+                    image.Save(output_file);
+                    Console.WriteLine(" Results saved in " + output_file + "\n");
+                }
+            }
+            else 
+            {
+                var errorDetails = ImageAnalysisErrorDetails.FromResult(result);
+                Console.WriteLine(" Analysis failed.");
+                Console.WriteLine($"    Error reason: {errorDetails.Reason}");                
+                Console.WriteLine($"    Error code: {errorDetails.ErrorCode}");
+                Console.WriteLine($"    Error message: {errorDetails.Message}\n");
+            }
         }
-
-
     }
 }
